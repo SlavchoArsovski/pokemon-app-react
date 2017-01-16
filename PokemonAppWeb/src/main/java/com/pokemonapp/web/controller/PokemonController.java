dcbf47f0d0b6786ea16d3e72c7cb229c8e320242
Slavcho Arsovski <slavcho.arsovski@netcetera.com>
2017-01-16 02:41:59 +0100

    initial rest config

--- PokemonAppWeb/src/main/java/com/pokemonapp/web/controller/PokemonController.java
+++ PokemonAppWeb/src/main/java/com/pokemonapp/web/controller/PokemonController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -59,10 +60,10 @@ public class PokemonController {
   }
 
   @RequestMapping(
-      value = "/deletePokemon",
+      value = "/deletePokemon/{pokemonId}",
       produces = MediaType.APPLICATION_JSON_VALUE,
       method = RequestMethod.DELETE)
-  public ResponseEntity<Pokemon> deletePokemon(@RequestParam Long pokemonId) {
+  public ResponseEntity<Pokemon> deletePokemon(@PathVariable Long pokemonId) {
     pokemonRepository.delete(pokemonId);
     return new ResponseEntity<Pokemon>(HttpStatus.NO_CONTENT);
   }
